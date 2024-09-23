915b6c71dc41268af188d8793faaa8d76e69ca2f
ashutosh-uzumaki <ashutosh.dev18@gmail.com>
2024-09-23 07:47:09 +0530

    solved two sum problem

---
+++ hashing/TwoSum.java
@@ -0,0 +1,21 @@
+package hashing;
+import java.util.*;
+
+public class TwoSum {
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+            int n = nums.length;
+    
+            for (int i = 0; i < n; i++) {
+                int complement = target - nums[i];
+                if (numMap.containsKey(complement)) {
+                    return new int[]{numMap.get(complement), i};
+                }
+                numMap.put(nums[i], i);
+            }
+    
+            return new int[]{};
+        }
+    }
+}
