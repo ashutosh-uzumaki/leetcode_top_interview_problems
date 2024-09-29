fbff8c7c75d658fe25651ddeaeeb02d6aa3b98d3
ashutosh-uzumaki <ashutosh.dev18@gmail.com>
2024-09-29 19:25:38 +0530

    remove duplicates from the sorted array

---
+++ linked_list/RemoveDuplicates.java
@@ -0,0 +1,30 @@
+public class RemoveDuplicates{
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummy = new ListNode(-1);
+        ListNode tail = dummy;
+        ListNode curr = head;
+        while(curr != null){
+            if(curr.next != null && curr.val == curr.next.val){
+                int val = curr.val;
+                while(curr != null && curr.val == val){
+                    curr = curr.next;
+                }
+            }else{
+                tail.next = curr;
+                tail = curr;
+                curr = curr.next;
+            }
+        }
+        tail.next = null;
+        return dummy.next;
+    }
+}
+}
