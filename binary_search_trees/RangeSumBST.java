735eff42f0f1ee0ae73e9b32d8273d78fa606d47
ashutosh-uzumaki <ashutosh.dev18@gmail.com>
2024-10-05 18:32:02 +0530

    solved sum of nodes in a binary search tree within the given range

---
+++ binary_search_trees/RangeSumBST.java
@@ -0,0 +1,37 @@
+package binary_search_trees;
+class TreeNode {
+         int val;
+         TreeNode left;
+         TreeNode right;
+         TreeNode() {}
+         TreeNode(int val) { this.val = val; }
+         TreeNode(int val, TreeNode left, TreeNode right) {
+             this.val = val;
+             this.left = left;
+             this.right = right;
+         }
+     }
+public class RangeSumBST {
+    class Solution {
+        int sum = 0;
+        public int rangeSumBST(TreeNode root, int low, int high) {
+            if(root == null){
+                return 0;
+            }
+    
+            if(root.val >= low && root.val <= high){
+                sum += root.val;
+            }
+    
+            if(root.left != null && root.val >= low){
+                rangeSumBST(root.left, low, high);
+            }
+    
+            if(root.right != null && root.val <= high){
+                rangeSumBST(root.right, low, high);
+            }
+    
+            return sum;
+        }
+    }
+}
