a321fd12ea976d6c4b081cd43a41d523c4c6a204
ashutosh-uzumaki <ashutosh.dev18@gmail.com>
2024-10-05 09:00:02 +0530

    solved binary tree inversion

---
+++ binary_trees/InvertBinaryTree.java
@@ -0,0 +1,20 @@
+package binary_trees;
+
+public class InvertBinaryTree {
+    class Solution {
+        public TreeNode invertTree(TreeNode root) {
+            if(root == null){
+                return null;
+            }
+    
+            TreeNode left = invertTree(root.left);
+            TreeNode right = invertTree(root.right);
+            TreeNode temp = left;
+            root.left = right;
+            root.right = temp;
+    
+    
+            return root;
+        }
+    }
+}
