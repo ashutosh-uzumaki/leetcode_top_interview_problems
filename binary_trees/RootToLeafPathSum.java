479a86c9b92e42e1dc2ea387166c468ddc0eb4a6
ashutosh-uzumaki <ashutosh.dev18@gmail.com>
2024-10-05 18:23:38 +0530

    solved check if sum exists from root to leaf

---
+++ binary_trees/RootToLeafPathSum.java
@@ -0,0 +1,39 @@
+package binary_trees;
+
+public class RootToLeafPathSum {
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        return helper(root, targetSum);
+    }
+
+    public boolean helper(TreeNode root, int targetSum){
+        if(root == null){
+            return false;
+        }
+        targetSum -= root.val;
+        if(root.left == null && root.right == null){
+            if(targetSum == 0){
+                return true;
+            }
+        }
+        boolean left = helper(root.left, targetSum);
+        boolean right = helper(root.right, targetSum);
+        return left || right;
+    }
+}
+}
