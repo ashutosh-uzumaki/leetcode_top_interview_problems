181eca2963a94f17c6874b8707d6209a942f4ba1
ashutosh-uzumaki <ashutosh.dev18@gmail.com>
2024-12-07 11:31:27 +0530

    solved minimum size subarray sum

---
+++ sliding_window/MinimumSizeSubarraySum.java
@@ -0,0 +1,22 @@
+package sliding_window;
+
+public class MinimumSizeSubarraySum {
+    class Solution {
+        public int minSubArrayLen(int target, int[] nums) {
+            int minLength = Integer.MAX_VALUE;
+            int left = 0, right = 0;
+            int n = nums.length;
+            int currSum = 0;
+            while(right < n){
+                currSum += nums[right];
+                while(left <= right && currSum >= target){
+                    minLength = Math.min(right - left + 1, minLength);
+                    currSum -= nums[left];
+                    left++;
+                }
+                right++;
+            }
+            return minLength == Integer.MAX_VALUE ? 0 : minLength;
+        }
+    }
+}
