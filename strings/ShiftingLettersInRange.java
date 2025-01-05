3dacf3618bb18a8417b7e8e75578d3600766088d
ashutosh-uzumaki <ashutosh.dev18@gmail.com>
2025-01-05 17:28:53 +0530

    solved lc daily

---
+++ strings/ShiftingLettersInRange.java
@@ -0,0 +1,40 @@
+public class ShiftingLetters {
+    class Solution {
+        public String shiftingLetters(String s, int[][] shifts) {
+            int n = s.length();
+            int[] diff = new int[n];
+            for(int[] shift: shifts){
+                int left = shift[0];
+                int right = shift[1];
+                int dir = shift[2];
+    
+                int x = 1;
+                if(dir == 0){
+                    x = -1;
+                }
+    
+                diff[left] += x;
+                if(right + 1 < n){
+                    diff[right + 1] -= x;
+                }
+            }
+    
+            for(int i=1; i<n; i++){
+                diff[i] = diff[i-1] + diff[i];
+            }
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i<n; i++){
+                int shift = diff[i] % 26;
+                if(shift < 0){
+                    shift += 26;
+                }
+    
+              char ch = (char)(((s.charAt(i) - 'a') + shift) % 26 + 'a');
+            //   System.out.println(ch);
+              sb.append(ch); 
+            }
+    
+            return sb.toString();
+        }
+    }
+}
